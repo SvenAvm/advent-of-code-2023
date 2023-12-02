703efff0f33718d56c5cb497261864dc516a5d6e
SvenAvm <sven.avm@gmail.com>
2023-12-02 15:25:38 +0100

    day 2

--- Day 2/run.py
+++ Day 2/run.py
@@ -1,15 +1,14 @@
 import utils
 
 
-starting_games_high = utils.parse_high_game_scores_from_file("input.txt")
-starting_games_low = utils.parse_low_game_scores_from_file("input.txt")
+high = utils.parse_high_game_scores_from_file("input.txt")
 
-filtered_games = utils.filter_games_by_possible_high_scores(starting_games_high, utils.possible_cubes)
+filtered_games = utils.filter_games_by_possible_high_scores(high, utils.possible_cubes)
 
 
 print(f"""
-Solution to task 1: {utils.get_solution_1(filtered_games)}
-Solution to task 2: {utils.get_solution_2(starting_games_high)}
+Solution to task 1: {utils.get_solution_1(utils.filter_games_by_possible_high_scores(high, utils.possible_cubes))}
+Solution to task 2: {utils.get_solution_2(high)}
 """)
 
 
